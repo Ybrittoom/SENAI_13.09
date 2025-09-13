800059dcb7c2485b4458286e053175e4964e97d5
Yago <150179517+Ybrittoom@users.noreply.github.com>
2025-09-13 13:59:48 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,39 +1,95 @@
 const express = require('express')
 const app = express()
-const PORT = 8081
+const PORT = 3000
 
-app.get('/operacao/:tipo', (req, res) => {
+app.get('/soma/:numUm/:numDois', (req, res) => {
     try {
-        const { tipo } = req.params
-        const { numUm, numDois } = req.query
+        const { numUm, numDois } = req.params
 
-        if( isNaN(numUm) || numUm == undefined || isNaN(numDois) || numDois == undefined) {
-            return console.log('Dados sao obrigatorios')
+        if (numUm == undefined || numDois == undefined || isNaN(numUm) || isNaN(numDois)) {
+            return res.status(400).send('Dados numUm e numDois sao obrigatorios e precisa ser numero')
+        }
+
+        //os numeros vem em tipo string nas URL, tem que transformar em Numeros inteiros
+        const numUmInt = parseInt(numUm)
+        const numDoisInt = parseInt(numDois)
+        res.status(200).send(
+            `<h1>
+                Ola, a soma dos numeros ${numUmInt} e numero ${numDoisInt} é: ${numUmInt + numDoisInt}!
+            </h1>`
+        )
+    } catch (error) {
+        console.error(`Erro ao processar a soma dos numeros: ${error}`)
+        res.status(500).send('Erro interno no servidor!')
+    }
+})
+
+//rota para subtraçao
+app.get('/subtracao/:numUm/:numDois', (req, res) => {
+    try {
+        const { numUm, numDois } = req.params
+
+        if ( numUm == undefined || numDois == undefined || isNaN(numUm) || isNaN(numDois)) {
+            return res.status(400).send('Dados numUm e numDois sao obrigatorios e precisa ser numero')
         }
 
         const numUmInt = parseInt(numUm)
         const numDoisInt = parseInt(numDois)
-        let resultado;
+        const resultado = numUmInt - numDoisInt
+        res.status(200).send(`
+            <h1>
+                Ola, a subtraçao dos numeros ${numUmInt} e ${numDoisInt} é : ${resultado}
+            </h1>
+        `)
+    } catch (error) {
+        console.error('Erro ao processar a subtraçao: ', error)
+        res.status(500).send('Erro interno no servidor')
+    }
+})
 
-        if ( tipo == 'soma' ) {
-            resultado = numUmInt + numDoisInt
-        } else if ( tipo == 'subtracao') {
-            resultado = numUmInt - numDoisInt
-        } else if ( tipo == 'multiplicacao' ) {
-            resultado = numUmInt * numDoisInt
-        } else {
-            resultado = numUmInt / numDoisInt
+//rota da multiplicaçao
+app.get('/multiplicacao/:numUm/:numDois', (req, res) => {
+    try {
+        const { numUm, numDois } = req.params
+
+        if ( numUm == undefined || numDois == undefined || isNaN(numUm) || isNaN(numDois)) {
+            return res.status(400).send('Dados numUm e numDois sao obrigatorios e precisa ser numero')
         }
 
+        const numUmInt = parseInt(numUm)
+        const numDoisInt = parseInt(numDois)
+        const resultado = numUmInt * numDoisInt
         res.status(200).send(
             `<h1>
-                O resultado é : ${resultado}
+                A multiplicaçao dos numeros ${numUmInt} e ${numDoisInt} é: ${resultado}
             </h1>`
         )
-        
     } catch (error) {
-        console.error('Erro ao processar os resultados', error)
+        console.error('Erro ao processar a multiplicaçao', error)
         res.status(500).send('Erro interno no servidor!')
+    }
+})
+
+//rota para divisao
+app.get('/divisao/:numUm/:numDois', (req, res) => {
+    try {
+        const { numUm, numDois } = req.params
+
+        if ( numUm == undefined || numDois == undefined || isNaN(numUm) || isNaN(numDois)) {
+            return res.status(400).send('Dados numUm e numDois sao obrigatorios e precisa ser numero')
+        }
+
+        const numUmInt = parseInt(numUm)
+        const numDoisInt = parseInt(numDois)
+        const resultado  = numUm / numDois 
+        res.status(200).send(
+            `<h1>
+                A divisao dos numeros ${numUmInt} e ${numDoisInt} é: ${resultado}
+            </h1>`
+        )
+    } catch (error) {
+        console.error('Erro ao processar a divisao dos numero:', error)
+        res.status(500).send('Erro interno no servidor')
     }
 })
 
