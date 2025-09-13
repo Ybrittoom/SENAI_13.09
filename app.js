6ba654c61bd2ca110de725ebabf3d9311632f3f0
Yago Emanuel B. De Moura <yagoemanuel99@gmail.com>
2025-09-13 12:44:43 -0300

    ..

--- app.js
+++ app.js
@@ -1,31 +1,16 @@
 const express = require('express')
 const app = express()
-const PORT = 8080
+const PORT = 8081
 
-app.get('/calculadora', (req, res) => {
-    const { operacao, numUm, numDois } = req.query
+app.get('/operacao/:tipo', (req, res) => {
+    try {
+        const { tipo } = req.params
 
-    const numUmInt = parseInt(numUm)
-    const numDoisInt = parseInt(numDois)
-    let resultado;
-
-    if ( operacao == 'soma') {
-        resultado = numUmInt + numDoisInt
-    } else if ( operacao == 'subtracao') {
-        resultado = numUmInt - numUmInt
-    } else if ( operacao == 'multiplicacao') {
-        resultado = numUmInt * numDoisInt
-    } else {
-        resultado = numUmInt / numDoisInt
+        
+    } catch (error) {
+        
     }
-
-    res.status(200).send(
-        `<h1>
-            O resultado é : ${resultado}
-        </h1>`
-    )
 })
-
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`)
