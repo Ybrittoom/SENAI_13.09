d88aa115773cd0565e094b05cd4b4567472d1e0d
Yago Emanuel B. De Moura <yagoemanuel99@gmail.com>
2025-09-13 12:53:57 -0300

    :sparkles: exercicio 3 finalizado

--- app.js
+++ app.js
@@ -5,10 +5,35 @@ const PORT = 8081
 app.get('/operacao/:tipo', (req, res) => {
     try {
         const { tipo } = req.params
+        const { numUm, numDois } = req.query
 
+        if( isNaN(numUm) || numUm == undefined || isNaN(numDois) || numDois == undefined) {
+            return console.log('Dados sao obrigatorios')
+        }
+
+        const numUmInt = parseInt(numUm)
+        const numDoisInt = parseInt(numDois)
+        let resultado;
+
+        if ( tipo == 'soma' ) {
+            resultado = numUmInt + numDoisInt
+        } else if ( tipo == 'subtracao') {
+            resultado = numUmInt - numDoisInt
+        } else if ( tipo == 'multiplicacao' ) {
+            resultado = numUmInt * numDoisInt
+        } else {
+            resultado = numUmInt / numDoisInt
+        }
+
+        res.status(200).send(
+            `<h1>
+                O resultado é : ${resultado}
+            </h1>`
+        )
         
     } catch (error) {
-        
+        console.error('Erro ao processar os resultados', error)
+        res.status(500).send('Erro interno no servidor!')
     }
 })
 
